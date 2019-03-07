0933878fc7aeda980258bbef014aa738aa82d874
lixiang.yang <lxyang6688@163.com>
2019-03-07 23:56:23 +0800

    新加容器文件

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,32 @@
     <!--<img alt="Vue logo" src="./assets/logo.png">-->
     <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
     <!--<layout-train></layout-train>-->
-    <layout-train-page></layout-train-page>
+    <!--<layout-train-page></layout-train-page>-->
+    <container-train></container-train>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import LayoutTrain from './components/LayoutTrain'
-import LayoutTrainPage from './components/LayoutTrainPage'
+// import LayoutTrain from './components/LayoutTrain'
+// import LayoutTrainPage from './components/LayoutTrainPage'
+import ContainerTrain from './components/ContainerTrain'
+
 
 export default {
   name: 'app',
   components: {
     // HelloWorld,
-    LayoutTrain,
-    LayoutTrainPage
+    // LayoutTrain,
+    // LayoutTrainPage,
+    ContainerTrain
   }
 }
 </script>
 
 <style>
   body {
-    margin: 0px;
+    margin: 0;
   }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -32,7 +36,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 0px;
+  margin-top: 0;
 }
 </style>
 
---
+++ src/components/ContainerTrain.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ContainerTrain"
+    }
+</script>
+
+<style scoped>
+
+</style>
